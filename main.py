17e3e9b90d7a10bab78eb85d6ffd4dd0d95d7529
windzer99311 <windzergamer@gmail.com>
2026-01-21 00:56:49 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+@app.get("/search")
+def get_stream(url: str):
+    api = "https://api.vidssave.com/api/contentsite_api/media/parse"
+
+    data = {
+        "auth": "20250901majwlqo",
+        "domain": "api-ak.vidssave.com",
+        "origin": "source",
+        "link": url,
+    }
+
+    r = requests.post(api, data=data)
+    return r.text
